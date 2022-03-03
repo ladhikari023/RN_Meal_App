fc31ab51ecfe45933b0dd435613f85eb10e611e2
Laxman Adhikari <ladhikari023@unm.edu>
2022-03-03 16:42:29 -0700

    First Commit

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+function MealDetailScreen() {
+  return (
+    <View style={styles.screen}>
+        <Text>Category Meals Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
+
+export default MealDetailScreen
